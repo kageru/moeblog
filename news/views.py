9906fd91873b83c7d3ab872572fa02f4bcc9e4b4
daf276 <daf276@gmx.de>
2017-11-15 23:42:50 +0100

    Can now view posts

--- news/views.py
+++ news/views.py
@@ -2,9 +2,18 @@ from django.shortcuts import render
 from .models import Post
 # Create your views here.
 
+
 def index(request):
     latest_post_list = Post.objects.order_by('-id')
 
     context = {'latest_post_list': latest_post_list}
 
     return render(request, 'news/news.html', context)
+
+
+def detail(request, post_id):
+    post = Post.objects.get(pk=post_id)
+
+    context = {'post': post}
+
+    return render(request, 'news/details.html', context)
