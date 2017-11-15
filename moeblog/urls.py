4d7bfd10d23558946d8e71bbfe6bcd02a59d8831
daf276 <daf276@gmx.de>
2017-11-15 20:39:31 +0100

    Faq + Team sites loading, stylesheet

--- moeblog/urls.py
+++ moeblog/urls.py
@@ -13,11 +13,25 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.shortcuts import render
 from django.urls import path
 from django.conf.urls import include, url
 from django.contrib import admin
+from news import views
+
+
+def faq(request):
+    return render(request, 'faq/faq.html')
+
+
+def team(request):
+    return render(request, 'team/team.html')
+
 
 urlpatterns = [
+    url(r'^$', views.index, name='index'),
+    url(r'^faq/', faq, name='faq'),
+    url(r'^team/', team, name='team'),
     url(r'^news/', include('news.urls')),
     url(r'^admin/', admin.site.urls),
 ]
--- news/views.py
+++ news/views.py
@@ -2,8 +2,5 @@ from django.shortcuts import render
 
 # Create your views here.
 
-from django.http import HttpResponse
-
-
 def index(request):
-    return render(request, 'header.html')
+    return render(request, 'news/news.html')
